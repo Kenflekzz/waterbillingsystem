3b8680e70869fcfc62295fea61bdc014d4f73bc1
Kenflekzz <kenflekzz@gmail.com>
2026-04-07 07:15:23 +0800

    added:  Load PayMongo secrets from Render Secret Files

--- public/index.php
+++ public/index.php
@@ -5,6 +5,21 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+// Load PayMongo secrets from Render Secret Files
+$secretsFile = '/etc/secrets/paymongo';
+if (file_exists($secretsFile)) {
+    $lines = file($secretsFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos($line, '=') !== false && strpos($line, '#') !== 0) {
+            list($key, $value) = explode('=', $line, 2);
+            $key = trim($key);
+            $value = trim($value);
+            $_ENV[$key] = $value;
+            putenv("$key=$value");
+        }
+    }
+}
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
