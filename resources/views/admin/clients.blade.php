4afe28f6c25edd7363d19f26d7bbe86b454c3f15
Kenflekzz <kenflekzz@gmail.com>
2026-04-12 22:51:35 +0800

    fix: remove onclick button

--- resources/views/admin/clients.blade.php
+++ resources/views/admin/clients.blade.php
@@ -117,7 +117,7 @@
                                 <form action="{{ route('admin.clients.destroy', $client->id) }}" method="POST" class="d-inline delete-client-form">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-sm btn-danger" title="Delete" onclick="return confirm('Are you sure you want to delete this client?');">
+                                    <button type="button" class="btn btn-sm btn-danger" title="Delete">
                                         <i class="fas fa-trash-alt"></i>
                                     </button>
                                 </form>
