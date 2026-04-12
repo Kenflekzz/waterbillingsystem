0c729789ca44555168dc4ea03e789b9fb15b0b34
Kenflekzz <kenflekzz@gmail.com>
2026-04-13 06:08:54 +0800

    fix: use env variables for Mocean config

--- config/mocean.php
+++ config/mocean.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'token'  => 'apit-57YcVAhYrpXDp2r8PEpOOMaNsvTeNUxG-SlIBL',
-    'sender' => 'MYAPP',
-    'url'    => 'https://rest.moceanapi.com/rest/2/sms',  // ✅ No space
+    'token'  => env('MOCEAN_API_TOKEN', 'apit-57YcVAhYrpXDp2r8PEpOOMaNsvTeNUxG-SlIBL'),
+    'sender' => env('MOCEAN_SENDER', 'MYAPP'),
+    'url'    => env('MOCEAN_URL', 'https://rest.moceanapi.com/rest/2/sms'),
 ];
